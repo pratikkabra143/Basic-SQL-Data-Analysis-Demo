350ee82690b5b0f4d3e043896cd4e06142a6b638
pratikkabra143 <pratikkabra2000@gmail.com>
2025-09-28 11:40:28 +0530

    query 3

--- analysis.sql
+++ analysis.sql
@@ -13,3 +13,9 @@ FROM orders o
 JOIN orderdetails od ON o.orderNumber = od.orderNumber
 GROUP BY o.customerNumber
 ORDER BY total_spent DESC;
+
+-- 3. INNER JOIN
+SELECT o.orderNumber, c.customerName, o.orderDate
+FROM orders o
+INNER JOIN customers c ON o.customerNumber = c.customerNumber
+ORDER BY o.orderDate DESC;
