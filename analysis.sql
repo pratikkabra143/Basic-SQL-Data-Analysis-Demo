cba31bfdf6e41b5b80c171059cf7b16cf7879b62
pratikkabra143 <pratikkabra2000@gmail.com>
2025-09-28 11:38:35 +0530

    query 2

--- analysis.sql
+++ analysis.sql
@@ -6,3 +6,10 @@ SELECT customerNumber, customerName, city, country
 FROM customers
 WHERE country = 'USA'
 ORDER BY city ASC;
+
+-- 2. GROUP BY + Aggregate Function (SUM)
+SELECT o.customerNumber, SUM(od.quantityOrdered * od.priceEach) AS total_spent
+FROM orders o
+JOIN orderdetails od ON o.orderNumber = od.orderNumber
+GROUP BY o.customerNumber
+ORDER BY total_spent DESC;
